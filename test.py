9445f3bd523bede601903b87b2e1b2504fab52c2
aaronsharpe <aaronlsharpe@gmail.com>
2024-06-22 10:56:14 -0700

    retrying on error

--- test.py
+++ test.py
@@ -5,7 +5,14 @@ import datetime
 if __name__ == '__main__':
     try:
         while True:
-            _, temp_f = read_temp()
+            temp_f = None
+            while temp_f is None:
+                try:
+                    _, temp_f = read_temp()
+                except Exception as e:
+                    print(f'Error reading temperature: {e}')
+                    time.sleep(1)
+
             current_time = datetime.datetime.fromtimestamp(time.time())
             readable_time = current_time.strftime('%Y-%m-%d %H:%M:%S')
             print(f'{readable_time}; Temperature: {temp_f:.2f}°F')
