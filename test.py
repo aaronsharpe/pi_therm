e2ce85ef0835ecc59fe88fa6dc7adcc9580cf92e
aaronsharpe <aaronlsharpe@gmail.com>
2024-06-22 00:15:07 -0700

    update test

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
 from thermometer import read_temp
+import time
+import datetime
 
 if __name__ == '__main__':
-    print(read_temp())
+    try:
+        while True:
+            _, temp_f = read_temp()
+            current_time = datetime.datetime.fromtimestamp(time.time())
+            readable_time = current_time.strftime('%Y-%m-%d %H:%M:%S')
+            print(f'{readable_time}; Temperature: {temp_f:.1f}°F')
+            time.sleep(30)  
+    except KeyboardInterrupt:
+        pass
